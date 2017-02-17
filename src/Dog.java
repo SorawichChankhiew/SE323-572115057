afdd3b20c7c527d3e1b57eac9f3a5ded8115001a
droppdeadz <dolawat.wannapira@gmail.com>
2017-02-17 09:47:37 +0700

    572115022

---
+++ src/Dog.java
@@ -0,0 +1,17 @@
+/**
+ * Created by droppdeadz on 17-Feb-17.
+ */
+public class Dog {
+    String breed;
+    int ageC;
+    String color;
+
+    void barking() {
+    }
+
+    void hungry() {
+    }
+
+    void sleeping() {
+    }
+}
